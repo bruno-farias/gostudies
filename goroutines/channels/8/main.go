9909996a8eba27170261fddd081f97e201725f8e
Bruno Farias <brunofarias1@gmail.com>
2017-02-12 19:38:06 -0200

    fan out

---
+++ goroutines/channels/8/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+    c := generate(5, 10)
+
+    // fan out
+    d1 := divide(c)
+    d2 := divide(c)
+
+    // fan in
+
+    fmt.Println(d1)
+    fmt.Println(d2)
+
+}
+
+func generate(numbers ...int) chan int {
+    channel := make(chan int)
+
+    go func() {
+	for _, n := range numbers {
+	    channel <- n
+	}
+	close(channel)
+    }()
+    return channel
+}
+
+func divide(input chan int) chan int  {
+    channel := make(chan int)
+
+    go func() {
+	for number := range input {
+	    channel <- number / 2
+	}
+	close(channel)
+    }()
+
+    return channel
+}
